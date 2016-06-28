7a76473387961b8abcf12e5e0351e4118edb28c9
kosuke shimofuji <kosuke.shimofuji@gmail.com>
2016-06-28 12:55:34 +0900

    add installed apps for defend model

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'open_multi_stack.apps.OpenMultiStackConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
