bd87592d553bcff313d86464a2820b111e13d2a4
kosuke shimofuji <kosuke.shimofuji@gmail.com>
2016-07-22 10:41:37 +0900

    cleanup django project

--- django_project/open_multi_stack/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import OpenStackAccount
-
-@admin.register(OpenStackAccount)
-class OpenStackAccount(admin.ModelAdmin):
-    pass
-
--- django_project/open_multi_stack/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OpenMultiStackConfig(AppConfig):
-    name = 'open_multi_stack'
