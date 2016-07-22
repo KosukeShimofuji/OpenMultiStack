400348a4a1f73d3ade79ca60f75bef2685123211
kosuke shimofuji <kosuke.shimofuji@gmail.com>
2016-07-22 17:47:56 +0900

    sort field of account table on admin page

--- django_project/open_multi_stack/admin.py
+++ django_project/open_multi_stack/admin.py
@@ -1,4 +1,10 @@
 from open_multi_stack.models import Account
 from django.contrib import admin
 
-admin.site.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    fields = ['provider', 'username', 'password', 'tenantname', 'tenant_id', 
+            'auth_url', 'version', 'status']
+
+admin.site.register(Account, AccountAdmin)
+
+
