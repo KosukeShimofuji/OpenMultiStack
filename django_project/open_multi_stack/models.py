a428fdc6bcc2b04d5d0232ae7ed132b36b642bfb
kosuke shimofuji <kosuke.shimofuji@gmail.com>
2016-07-25 11:49:01 +0900

    update model

--- django_project/open_multi_stack/models.py
+++ django_project/open_multi_stack/models.py
@@ -17,15 +17,19 @@ class Account(models.Model):
     )
     status = models.CharField(choices=STATUS_SET, default=STATUS_AVAILABLE, max_length=12)
     provider    = models.CharField(max_length=128)
+    regist_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.tenantname + '_' + self.provider
 
 class Instance(models.Model):
-    name    = models.CharField(max_length=128)
-    ip      = models.CharField(max_length=128)
-    key     = models.CharField(max_length=128)
-    account = models.ForeignKey(Account)
+    name        = models.CharField(max_length=128)
+    ip_addr     = models.CharField(max_length=128)
+    username    = models.CharField(max_length=128)
+    password    = models.CharField(max_length=128)
+    key         = models.TextField(blank=True, null=False)
+    regist_date = models.DateField(auto_now_add=True)
+    account     = models.ForeignKey(Account)
 
     def __str__(self):
         return self.name + '_' + self.ip + '_' + self.account
