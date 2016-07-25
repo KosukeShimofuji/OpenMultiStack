7150b4254b7bbefcc0d11ffba000ea53a42a6ac7
kosuke shimofuji <kosuke.shimofuji@gmail.com>
2016-07-25 18:57:33 +0900

    add controller

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -13,15 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from . import views
+from open_multi_stack.urls import router as open_multi_stack_router
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    # open_multi_stack.urlsをincludeする
+    url(r'^api/', include(open_multi_stack_router.urls)),
 ]
-
-#urlpatterns = [
-#    url(r'^$', views.index, name='index'),
-#]
-
