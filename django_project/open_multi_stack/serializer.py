1ea92357c62cce70326d828b2531aad74139c5c5
kosuke shimofuji <kosuke.shimofuji@gmail.com>
2016-07-26 10:59:00 +0900

    add read_only_fileds

--- django_project/open_multi_stack/serializer.py
+++ django_project/open_multi_stack/serializer.py
@@ -4,6 +4,6 @@ from .models import Queue
 class QueueSerializer(serializers.ModelSerializer):
     class Meta:
         model = Queue
-        fields = ('id', 'op', 'status', 'regist_datetime')
-        read_only_fields = ('status',)
+        fields = ('id', 'status', 'regist_datetime', 'instance')
+        read_only_fields = ('status','instance')
 
