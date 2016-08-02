9ac1e55478059a6b5e3b94f5426d87b5e56818b7
kosuke shimofuji <kosuke.shimofuji@gmail.com>
2016-08-02 14:25:23 +0900

    modified reciver

--- django_project/open_multi_stack/reciver.py
+++ django_project/open_multi_stack/reciver.py
@@ -2,6 +2,6 @@ from datetime import datetime
 
 def test(sender, instance, **kwargs):
     f = open("/tmp/test.txt", "a")
-    f.writelines("test " + datetime.now().strftime("%Y/%m/%d %H:%M:%S") + "\n")
+    f.writelines("test " + datetime.now().strftime("%Y/%m/%d %H:%M:%S") + ' ' + str(instance.id) + "\n")
     f.close()
 
