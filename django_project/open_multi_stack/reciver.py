f30350db1f250a3237c01e375cfbd251bcc48bc4
kosuke shimofuji <kosuke.shimofuji@gmail.com>
2016-08-02 14:31:45 +0900

    testcode for signal reciver

--- django_project/open_multi_stack/reciver.py
+++ django_project/open_multi_stack/reciver.py
@@ -1,7 +1,12 @@
 from datetime import datetime
+from time import sleep
 
 def test(sender, instance, **kwargs):
     f = open("/tmp/test.txt", "a")
     f.writelines("test " + datetime.now().strftime("%Y/%m/%d %H:%M:%S") + ' ' + str(instance.id) + "\n")
+    f.writelines("start signal reciver\n")
+    sleep(10)
+    f.writelines("end signal reciver\n")
     f.close()
+
 
